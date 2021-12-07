0860dc7fba39c9d369b0782ef6cfbf7487a70ec8
vincent <vincent.paturet@entur.org>
2021-12-07 13:38:03 +0100

    Add XPath validation

--- src/main/java/no/entur/antu/validator/xpath/ValidateNSRCodespace.java
+++ src/main/java/no/entur/antu/validator/xpath/ValidateNSRCodespace.java
@@ -1,14 +1,12 @@
 package no.entur.antu.validator.xpath;
 
+import no.entur.antu.Constants;
 import no.entur.antu.validator.ValidationReportEntrySeverity;
 
 public class ValidateNSRCodespace extends ValidateExactlyOne {
 
-    private static final String NSR_XMLNSURL = "http://www.rutebanken.org/ns/nsr";
-    private static final String NSR_XMLNS = "NSR";
-
     public ValidateNSRCodespace() {
-        super("codespaces/Codespace[Xmlns = '" + NSR_XMLNS + "' and XmlnsUrl = '" + NSR_XMLNSURL + "']",
+        super("codespaces/Codespace[Xmlns = '" + Constants.NSR_XMLNS + "' and XmlnsUrl = '" + Constants.NSR_XMLNSURL + "']",
                 "NSR Codespace must be declared (Xmlns=NSR, XmlnsUrl=http://www.rutebanken.org/ns/nsr). Any references to StopPlaces must point to data in the NSR namespace",
                 "Composite Frame",
                 ValidationReportEntrySeverity.ERROR);
