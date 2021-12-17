2886d50124bc1d0b8aa48cb6e46d0d30bc769cbd
vpaturet <46598384+vpaturet@users.noreply.github.com>
2021-12-17 13:34:55 +0100

    Fix NeTEx id repository (#26)

--- src/main/java/no/entur/antu/config/CacheConfig.java
+++ src/main/java/no/entur/antu/config/CacheConfig.java
@@ -1,7 +1,8 @@
 package no.entur.antu.config;
 
-import no.entur.antu.validator.id.IdVersion;
-import no.entur.antu.validator.id.LocalIdCache;
+import no.entur.antu.validator.id.NetexIdRepository;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.Codec;
 import org.redisson.codec.Kryo5Codec;
 import org.redisson.config.Config;
@@ -9,15 +10,11 @@ import org.redisson.jcache.configuration.RedissonConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.cache.Cache;
 import javax.cache.Caching;
-import javax.cache.configuration.Factory;
 import javax.cache.configuration.MutableConfiguration;
-import javax.cache.expiry.CreatedExpiryPolicy;
-import javax.cache.expiry.Duration;
-import javax.cache.expiry.ExpiryPolicy;
-import java.util.Map;
 import java.util.Set;
 
 @Configuration
@@ -43,6 +40,12 @@ public class CacheConfig {
         return redissonConfig;
     }
 
+    @Bean(destroyMethod = "shutdown")
+    @Profile("!test")
+    public RedissonClient redissonClient(Config redissonConfig) {
+        return Redisson.create(redissonConfig);
+    }
+
     @Bean
     public Cache<String, Set<String>> stopPlaceCache(Config redissonConfig) {
         MutableConfiguration<String, Set<String>> cacheConfig = new MutableConfiguration<>();
@@ -52,16 +55,7 @@ public class CacheConfig {
     }
 
     @Bean
-    public Cache<String, Map<String, IdVersion>> idVersionCache(Config redissonConfig) {
-        MutableConfiguration<String, Map<String, IdVersion>> cacheConfig = new MutableConfiguration<>();
-        var redissonCacheConfig = RedissonConfiguration.fromConfig(redissonConfig, cacheConfig);
-        cacheConfig.setExpiryPolicyFactory((Factory<ExpiryPolicy>) () -> new CreatedExpiryPolicy(Duration.ONE_HOUR));
-        var manager = Caching.getCachingProvider().getCacheManager();
-        return manager.createCache(ID_VERSION_CACHE_KEY, redissonCacheConfig);
-    }
-
-    @Bean
-    public LocalIdCache localIdCache(Cache<String, Map<String, IdVersion>> idVersionCache) {
-        return new LocalIdCache(idVersionCache);
+    public NetexIdRepository netexIdRepository(RedissonClient redissonClient) {
+        return new NetexIdRepository(redissonClient);
     }
 }
