63086d6597cdba7efa4b3ed8b3c4d5b851654732
vincent <vincent.paturet@entur.org>
2022-11-30 15:45:07 +0100

    Add support for flexible transport validation

---
+++ src/main/java/no/entur/antu/flex/config/TimetableDataFlexValidatorConfig.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *   https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ *
+ */
+
+package no.entur.antu.flex.config;
+
+import no.entur.antu.flex.validator.EnturFlexTimetableDataValidationTreeFactory;
+import no.entur.antu.organisation.OrganisationRepository;
+import no.entur.antu.validator.id.NetexIdValidator;
+import org.entur.netex.validation.validator.NetexValidator;
+import org.entur.netex.validation.validator.NetexValidatorsRunner;
+import org.entur.netex.validation.validator.ValidationReportEntryFactory;
+import org.entur.netex.validation.validator.id.NeTexReferenceValidator;
+import org.entur.netex.validation.validator.id.NetexIdUniquenessValidator;
+import org.entur.netex.validation.validator.id.ReferenceToValidEntityTypeValidator;
+import org.entur.netex.validation.validator.id.VersionOnLocalNetexIdValidator;
+import org.entur.netex.validation.validator.id.VersionOnRefToLocalNetexIdValidator;
+import org.entur.netex.validation.validator.schema.NetexSchemaValidator;
+import org.entur.netex.validation.validator.xpath.ValidationTreeFactory;
+import org.entur.netex.validation.validator.xpath.XPathValidator;
+import org.entur.netex.validation.xml.NetexXMLParser;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Configuration for validating flexible transport timetable data.
+ */
+@Configuration
+public class TimetableDataFlexValidatorConfig {
+
+    @Bean
+    public ValidationTreeFactory flexTimetableDataValidationTreeFactory(OrganisationRepository organisationRepository) {
+        return new EnturFlexTimetableDataValidationTreeFactory(organisationRepository);
+    }
+
+    @Bean
+    public XPathValidator flexTimetableDataXPathValidator(@Qualifier("flexTimetableDataValidationTreeFactory") ValidationTreeFactory validationTreeFactory, ValidationReportEntryFactory validationReportEntryFactory) {
+        return new XPathValidator(validationTreeFactory, validationReportEntryFactory);
+    }
+
+    @Bean
+    public NetexValidatorsRunner flexTimetableDataValidatorsRunner(NetexSchemaValidator netexSchemaValidator,
+                                                                   @Qualifier("flexTimetableDataXPathValidator") XPathValidator flexXPathValidator,
+                                                                   NetexIdValidator netexIdValidator,
+                                                                   VersionOnLocalNetexIdValidator versionOnLocalNetexIdValidator,
+                                                                   VersionOnRefToLocalNetexIdValidator versionOnRefToLocalNetexIdValidator,
+                                                                   ReferenceToValidEntityTypeValidator referenceToValidEntityTypeValidator,
+                                                                   NeTexReferenceValidator neTexReferenceValidator,
+                                                                   NetexIdUniquenessValidator netexIdUniquenessValidator) {
+        List<NetexValidator> netexValidators = List.of(flexXPathValidator, netexIdValidator, versionOnLocalNetexIdValidator, versionOnRefToLocalNetexIdValidator, referenceToValidEntityTypeValidator, neTexReferenceValidator, netexIdUniquenessValidator);
+        // do not ignore SiteFrame
+        NetexXMLParser netexXMLParser = new NetexXMLParser(Set.of());
+        return new NetexValidatorsRunner(netexXMLParser, netexSchemaValidator, netexValidators);
+    }
+
+}
