86610e614bb67eac0fd366ac222d66e991a11681
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-08-04 08:23:10 +0200

    Same stops in Journey Pattern validation

---
+++ src/main/java/no/entur/antu/validator/journeypattern/stoppoint/samestoppoints/SameStopPointsError.java
@@ -0,0 +1,44 @@
+package no.entur.antu.validator.journeypattern.stoppoint.samestoppoints;
+
+import java.util.List;
+import no.entur.antu.validator.ValidationError;
+
+public record SameStopPointsError(
+  RuleCode ruleCode,
+  List<String> journeyPatternIds
+)
+  implements ValidationError {
+  @Override
+  public String getRuleCode() {
+    return ruleCode.toString();
+  }
+
+  @Override
+  public String validationReportEntryMessage() {
+    return String.format(
+      "%s. [%s]",
+      ruleCode.getErrorMessage(),
+      String.join(", ", journeyPatternIds())
+    );
+  }
+
+  @Override
+  public String getEntityId() {
+    return null;
+  }
+
+  enum RuleCode implements no.entur.antu.validator.RuleCode {
+    SAME_STOP_POINT_IN_JOURNEY_PATTERNS("JourneyPatterns have same StopPoints");
+
+    private final String errorMessage;
+
+    RuleCode(String errorMessage) {
+      this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String getErrorMessage() {
+      return errorMessage;
+    }
+  }
+}
