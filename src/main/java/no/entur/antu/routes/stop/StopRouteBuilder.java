a51ba9dc0cb832af862483d1bc6612dcbacd66f0
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-09-26 13:04:54 +0200

    Removing commented out code.

--- src/main/java/no/entur/antu/routes/stop/StopRouteBuilder.java
+++ src/main/java/no/entur/antu/routes/stop/StopRouteBuilder.java
@@ -39,18 +39,12 @@ public class StopRouteBuilder extends BaseRouteBuilder {
     public void configure() throws Exception {
         super.configure();
 
-        from("quartz://antu/refreshStopPlaceCacheAtStartup?" + "?trigger.repeatCount=0")
-                .log(LoggingLevel.INFO, correlation() + "Refreshing stop place cache at startup")
-                .to("direct:refresh-stop-cache")
-                .routeId("refresh-stop-cache-at-startup");
-
         from("master:lockOnAntuRefreshStopCachePeriodically:quartz://antu/refreshStopPlaceCachePeriodically?" + quartzTrigger)
                 .log(LoggingLevel.INFO, correlation() + "Refreshing stop place cache periodically")
                 .to("direct:refresh-stop-cache")
                 .routeId("refresh-stop-cache-periodically");
 
         from("direct:refresh-stop-cache")
-                .log(LoggingLevel.INFO, correlation() + "Refreshing stop place cache")
                 .bean("stopPlaceRepository", "refreshCache")
                 .log(LoggingLevel.INFO, correlation() + "Refreshed stop place cache")
                 .routeId("refresh-stop-cache");
