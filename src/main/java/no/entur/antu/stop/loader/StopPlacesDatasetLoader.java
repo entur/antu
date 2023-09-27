6bd8c7a8695920d445d57f58d1aeb34a82f6b276
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-09-27 20:34:55 +0200

    Changing the stop place file.

--- src/main/java/no/entur/antu/stop/loader/StopPlacesDatasetLoader.java
+++ src/main/java/no/entur/antu/stop/loader/StopPlacesDatasetLoader.java
@@ -21,7 +21,7 @@ public class StopPlacesDatasetLoader {
     private final String currentStopPlacesFile;
     
     public StopPlacesDatasetLoader(MardukBlobStoreService mardukBlobStoreService,
-                                   @Value("${antu.netex.stop.current.filename:tiamat/Current_latest.zip}") String currentStopPlacesFile) {
+                                   @Value("${antu.netex.stop.current.filename:tiamat/CurrentAndFuture_latest.zip}") String currentStopPlacesFile) {
         this.mardukBlobStoreService = mardukBlobStoreService;
         this.currentStopPlacesFile = currentStopPlacesFile;
     }
