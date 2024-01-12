a13953375bf5667ae791d2df0f49b826abbc4d48
Mansoor Sajjad <mansoor.sajjad@entur.org>
2024-01-12 11:54:22 +0100

    Running prettier again after rebase.

--- src/main/java/no/entur/antu/stoptime/PassingTimes.java
+++ src/main/java/no/entur/antu/stoptime/PassingTimes.java
@@ -1,31 +1,40 @@
 package no.entur.antu.stoptime;
 
 public record PassingTimes(StopTime from, StopTime to) {
-    public boolean isValid() {
-        return from.isComplete() && from.isConsistent()
-                && to.isComplete() && to.isConsistent()
-                && from.isStopTimesIncreasing(to);
-    }
+  public boolean isValid() {
+    return (
+      from.isComplete() &&
+      from.isConsistent() &&
+      to.isComplete() &&
+      to.isConsistent() &&
+      from.isStopTimesIncreasing(to)
+    );
+  }
 
-    /**
-     * Returns time difference in seconds
-     */
-    public int getTimeDifference() {
-        return from.getStopTimeDiff(to);
-    }
+  /**
+   * Returns time difference in seconds
+   */
+  public int getTimeDifference() {
+    return from.getStopTimeDiff(to);
+  }
 
-    public boolean hasMinutesResolution() {
-        return from.isDepartureInMinutesResolution()
-                && to.isArrivalInMinutesResolution();
-    }
+  public boolean hasMinutesResolution() {
+    return (
+      from.isDepartureInMinutesResolution() && to.isArrivalInMinutesResolution()
+    );
+  }
 
-    public double minimumPossibleTimeDifference(int maxErrorSeconds) {
-        int timeDifferenceInSeconds = getTimeDifference();
-        return hasMinutesResolution() ? Math.max(timeDifferenceInSeconds - maxErrorSeconds, 1) : timeDifferenceInSeconds;
-    }
+  public double minimumPossibleTimeDifference(int maxErrorSeconds) {
+    int timeDifferenceInSeconds = getTimeDifference();
+    return hasMinutesResolution()
+      ? Math.max(timeDifferenceInSeconds - maxErrorSeconds, 1)
+      : timeDifferenceInSeconds;
+  }
 
-    public double maximumPossibleTimeDifference(int maxErrorSeconds) {
-        int timeDifferenceInSeconds = getTimeDifference();
-        return hasMinutesResolution() ? timeDifferenceInSeconds + maxErrorSeconds : timeDifferenceInSeconds;
-    }
+  public double maximumPossibleTimeDifference(int maxErrorSeconds) {
+    int timeDifferenceInSeconds = getTimeDifference();
+    return hasMinutesResolution()
+      ? timeDifferenceInSeconds + maxErrorSeconds
+      : timeDifferenceInSeconds;
+  }
 }
--- src/main/java/no/entur/antu/validator/AntuNetexValidator.java
+++ src/main/java/no/entur/antu/validator/AntuNetexValidator.java
@@ -1,52 +1,63 @@
 package no.entur.antu.validator;
 
-import org.entur.netex.validation.validator.*;
-import org.entur.netex.validation.validator.xpath.ValidationContext;
-
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.entur.netex.validation.validator.*;
+import org.entur.netex.validation.validator.xpath.ValidationContext;
 
 public abstract class AntuNetexValidator extends AbstractNetexValidator {
 
-    protected AntuNetexValidator(ValidationReportEntryFactory validationReportEntryFactory) {
-        super(validationReportEntryFactory);
-    }
+  protected AntuNetexValidator(
+    ValidationReportEntryFactory validationReportEntryFactory
+  ) {
+    super(validationReportEntryFactory);
+  }
 
-    protected abstract RuleCode[] getRuleCodes();
+  protected abstract RuleCode[] getRuleCodes();
 
-    protected void addValidationReportEntry(ValidationReport validationReport,
-                                            ValidationContext validationContext,
-                                            ValidationError validationError) {
+  protected void addValidationReportEntry(
+    ValidationReport validationReport,
+    ValidationContext validationContext,
+    ValidationError validationError
+  ) {
+    ValidationReportEntry validationReportEntry = createValidationReportEntry(
+      validationError.getRuleCode(),
+      findDataLocation(validationContext, validationError.getEntityId()),
+      validationError.validationReportEntryMessage()
+    );
 
-        ValidationReportEntry validationReportEntry = createValidationReportEntry(
-                validationError.getRuleCode(),
-                findDataLocation(validationContext, validationError.getEntityId()),
-                validationError.validationReportEntryMessage()
-        );
+    validationReport.addValidationReportEntry(validationReportEntry);
+  }
 
-        validationReport.addValidationReportEntry(validationReportEntry);
-    }
+  private static DataLocation findDataLocation(
+    ValidationContext validationContext,
+    String entityId
+  ) {
+    String fileName = validationContext.getFileName();
+    return validationContext
+      .getLocalIds()
+      .stream()
+      .filter(localId -> localId.getId().equals(entityId))
+      .findFirst()
+      .map(idVersion ->
+        new DataLocation(
+          idVersion.getId(),
+          fileName,
+          idVersion.getLineNumber(),
+          idVersion.getColumnNumber()
+        )
+      )
+      .orElse(new DataLocation(entityId, fileName, 0, 0));
+  }
 
-    private static DataLocation findDataLocation(ValidationContext validationContext, String entityId) {
-        String fileName = validationContext.getFileName();
-        return validationContext.getLocalIds().stream()
-                .filter(localId -> localId.getId().equals(entityId))
-                .findFirst()
-                .map(idVersion ->
-                        new DataLocation(
-                                idVersion.getId(),
-                                fileName,
-                                idVersion.getLineNumber(),
-                                idVersion.getColumnNumber()
-                        ))
-                .orElse(new DataLocation(entityId, fileName, 0, 0));
-    }
-
-    @Override
-    public final Set<String> getRuleDescriptions() {
-        return Arrays.stream(getRuleCodes())
-                .map(ruleCode -> createRuleDescription(ruleCode.toString(), ruleCode.getErrorMessage()))
-                .collect(Collectors.toSet());
-    }
+  @Override
+  public final Set<String> getRuleDescriptions() {
+    return Arrays
+      .stream(getRuleCodes())
+      .map(ruleCode ->
+        createRuleDescription(ruleCode.toString(), ruleCode.getErrorMessage())
+      )
+      .collect(Collectors.toSet());
+  }
 }
