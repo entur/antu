46b2b4e44a818dd0f4192a1e3ea0b4af96ced1d2
Mansoor Sajjad <mansoor.sajjad@entur.org>
2024-02-09 20:31:44 +0100

    Adding the basic integration test for TDD.

---
+++ src/main/java/no/entur/antu/flex/validation/validator/flexibleareavalidator/FlexibleAreaError.java
@@ -0,0 +1,43 @@
+package no.entur.antu.flex.validation.validator.flexibleareavalidator;
+
+import no.entur.antu.validator.ValidationError;
+
+public record FlexibleAreaError(
+  RuleCode ruleCode,
+  String flexibleAreaId
+)
+  implements ValidationError {
+
+  @Override
+  public String getRuleCode() {
+    return ruleCode.toString();
+  }
+
+  @Override
+  public String validationReportEntryMessage() {
+    return String.format(
+      "Invalid flexible area found with id %s",
+      flexibleAreaId()
+    );
+  }
+
+  @Override
+  public String getEntityId() {
+    return flexibleAreaId();
+  }
+
+  enum RuleCode implements no.entur.antu.validator.RuleCode {
+    INVALID_FLEXIBLE_AREA("Invalid flexible area");
+
+    private final String errorMessage;
+
+    RuleCode(String errorMessage) {
+      this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String getErrorMessage() {
+      return errorMessage;
+    }
+  }
+}
