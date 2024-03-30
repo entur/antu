2f04192651e4ffc5d3b35f97f0791d7ec2b7b194
Mansoor Sajjad <mansoor.sajjad@entur.org>
2024-03-30 08:52:55 +0100

    Validating the same quay refs are used in two consecutive stop points.

---
+++ src/main/java/no/entur/antu/validation/validator/journeypattern/stoppoint/samequayref/SameQuayRefContext.java
@@ -0,0 +1,44 @@
+package no.entur.antu.validation.validator.journeypattern.stoppoint.samequayref;
+
+import java.util.List;
+import no.entur.antu.model.QuayId;
+import no.entur.antu.model.ScheduledStopPointId;
+import no.entur.antu.validation.AntuNetexData;
+import org.rutebanken.netex.model.JourneyPattern;
+
+public record SameQuayRefContext(
+  String journeyPatternId,
+  ScheduledStopPointId scheduledStopPointId,
+  QuayId quayId
+) {
+  public boolean isValid() {
+    return scheduledStopPointId != null && quayId != null;
+  }
+
+  public static Builder builder(AntuNetexData antuNetexData) {
+    return new Builder(antuNetexData);
+  }
+
+  public static class Builder {
+
+    private final AntuNetexData antuNetexData;
+
+    private Builder(AntuNetexData antuNetexData) {
+      this.antuNetexData = antuNetexData;
+    }
+
+    public List<SameQuayRefContext> build(JourneyPattern journeyPattern) {
+      return AntuNetexData
+        .stopPointsInJourneyPattern(journeyPattern)
+        .map(ScheduledStopPointId::of)
+        .map(stopPointId ->
+          new SameQuayRefContext(
+            journeyPattern.getId(),
+            stopPointId,
+            antuNetexData.findQuayIdForScheduledStopPoint(stopPointId)
+          )
+        )
+        .toList();
+    }
+  }
+}
