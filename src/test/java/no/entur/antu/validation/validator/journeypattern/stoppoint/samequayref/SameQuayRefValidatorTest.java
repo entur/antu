48145d05b622d6ce7ad6f1e57c2c341aa068e043
Mansoor Sajjad <mansoor.sajjad@entur.org>
2024-03-30 08:52:55 +0100

    Validating the same quay refs are used in two consecutive stop points.

---
+++ src/test/java/no/entur/antu/validation/validator/journeypattern/stoppoint/samequayref/SameQuayRefValidatorTest.java
@@ -0,0 +1,134 @@
+package no.entur.antu.validation.validator.journeypattern.stoppoint.samequayref;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.Collections;
+import java.util.List;
+import no.entur.antu.model.QuayId;
+import no.entur.antu.model.ScheduledStopPointId;
+import no.entur.antu.netextestdata.NetexTestFragment;
+import no.entur.antu.validation.ValidationTest;
+import org.entur.netex.index.api.NetexEntitiesIndex;
+import org.entur.netex.validation.validator.ValidationReport;
+import org.junit.jupiter.api.Test;
+import org.rutebanken.netex.model.JourneyPattern;
+
+class SameQuayRefValidatorTest extends ValidationTest {
+
+  private ValidationReport runValidation(
+    NetexEntitiesIndex netexEntitiesIndex
+  ) {
+    return runValidationOnLineFile(
+      netexEntitiesIndex,
+      SameQuayRefValidator.class
+    );
+  }
+
+  @Test
+  void testNoStopPointsInJourneyPattern() {
+    NetexTestFragment testFragment = new NetexTestFragment();
+
+    JourneyPattern journeyPattern = testFragment
+      .journeyPattern()
+      .withId(1)
+      .withStopPointsInJourneyPattern(Collections.emptyList())
+      .create();
+
+    ValidationReport validationReport = runValidation(
+      testFragment
+        .netexEntitiesIndex()
+        .addJourneyPatterns(journeyPattern)
+        .create()
+    );
+
+    assertThat(validationReport.getValidationReportEntries().size(), is(0));
+  }
+
+  @Test
+  void testNoSameQuayRefOnStopPoints() {
+    NetexTestFragment testFragment = new NetexTestFragment();
+
+    JourneyPattern journeyPattern = testFragment
+      .journeyPattern()
+      .withId(1)
+      .withStopPointsInJourneyPattern(
+        List.of(
+          testFragment
+            .stopPointInJourneyPattern(1)
+            .withId(1)
+            .withScheduledStopPointId(1)
+            .create(),
+          testFragment
+            .stopPointInJourneyPattern(1)
+            .withId(2)
+            .withScheduledStopPointId(2)
+            .create()
+        )
+      )
+      .create();
+
+    mockGetQuayId(
+      new ScheduledStopPointId("TST:ScheduledStopPoint:1"),
+      new QuayId("TST:Quay:1")
+    );
+
+    mockGetQuayId(
+      new ScheduledStopPointId("TST:ScheduledStopPoint:2"),
+      new QuayId("TST:Quay:2")
+    );
+
+    ValidationReport validationReport = runValidation(
+      testFragment
+        .netexEntitiesIndex()
+        .addJourneyPatterns(journeyPattern)
+        .create()
+    );
+
+    assertThat(validationReport.getValidationReportEntries().size(), is(0));
+  }
+
+  @Test
+  void testSameQuayRefOnStopPoints() {
+    NetexTestFragment testFragment = new NetexTestFragment();
+
+    JourneyPattern journeyPattern = testFragment
+      .journeyPattern()
+      .withId(1)
+      .withStopPointsInJourneyPattern(
+        List.of(
+          testFragment
+            .stopPointInJourneyPattern(1)
+            .withId(1)
+            .withScheduledStopPointId(1)
+            .create(),
+          testFragment
+            .stopPointInJourneyPattern(1)
+            .withId(2)
+            .withScheduledStopPointId(2)
+            .create()
+        )
+      )
+      .create();
+
+    QuayId testQuayId1 = new QuayId("TST:Quay:1");
+
+    mockGetQuayId(
+      new ScheduledStopPointId("TST:ScheduledStopPoint:1"),
+      testQuayId1
+    );
+    mockGetQuayId(
+      new ScheduledStopPointId("TST:ScheduledStopPoint:2"),
+      testQuayId1
+    );
+
+    ValidationReport validationReport = runValidation(
+      testFragment
+        .netexEntitiesIndex()
+        .addJourneyPatterns(journeyPattern)
+        .create()
+    );
+
+    assertThat(validationReport.getValidationReportEntries().size(), is(1));
+  }
+}
