4c8cbdd7a5e1c5fd06232257f8cb8b6502d5ce2e
erlendev <erlend.vige@gmail.com>
2025-04-23 12:23:30 +0200

    Adds documentation to Agreement.java

--- src/main/java/no/entur/antu/validation/validator/organisation/Agreement.java
+++ src/main/java/no/entur/antu/validation/validator/organisation/Agreement.java
@@ -2,6 +2,10 @@ package no.entur.antu.validation.validator.organisation;
 
 import java.util.List;
 
+/**
+ * An agreement as defined by the agreement registry.
+ * Used to deserialize JSON responses fetched from the agreement registry.
+ */
 public class Agreement {
 
   private List<String> roleIds;
