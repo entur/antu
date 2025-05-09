cde6fe52cb495e4086aa7a2e5241bfaee4a3ada1
Vincent Paturet <vincent.paturet@entur.org>
2025-05-09 16:05:54 +0200

    Ignore scheduled stop points, fare zones and topographic places when importing stop place file

--- src/main/java/no/entur/antu/stop/loader/DefaultStopPlacesDatasetLoader.java
+++ src/main/java/no/entur/antu/stop/loader/DefaultStopPlacesDatasetLoader.java
@@ -1,15 +1,22 @@
 package no.entur.antu.stop.loader;
 
+import static org.entur.netex.validation.xml.NetexXMLParser.NETEX_NAMESPACE;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import no.entur.antu.exception.AntuException;
 import no.entur.antu.services.MardukBlobStoreService;
 import org.entur.netex.NetexParser;
 import org.entur.netex.index.api.NetexEntitiesIndex;
 import org.entur.netex.index.impl.NetexEntitiesIndexImpl;
+import org.entur.netex.validation.xml.SkippingXMLStreamReaderFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -60,11 +67,29 @@ public class DefaultStopPlacesDatasetLoader implements StopPlacesDatasetLoader {
     NetexEntitiesIndex index
   ) throws IOException {
     ZipEntry zipEntry = zipInputStream.getNextEntry();
-    NetexParser netexParser = new NetexParser();
+    NetexParser netexParser = new NetexParser(this::filteredXmlStreamReader);
     while (zipEntry != null) {
       byte[] allBytes = zipInputStream.readAllBytes();
       netexParser.parse(new ByteArrayInputStream(allBytes), index);
       zipEntry = zipInputStream.getNextEntry();
+    }
+  }
+
+  /**
+   * Ignore scheduled stop points, fare zones and topographic places.
+   */
+  private XMLStreamReader filteredXmlStreamReader(InputStream stream) {
+    try {
+      return SkippingXMLStreamReaderFactory.newXMLStreamReader(
+        stream,
+        Set.of(
+          new QName(NETEX_NAMESPACE, "scheduledStopPoints"),
+          new QName(NETEX_NAMESPACE, "fareZones"),
+          new QName(NETEX_NAMESPACE, "topographicPlaces")
+        )
+      );
+    } catch (XMLStreamException e) {
+      throw new AntuException("Cannot create XMLStreamReader", e);
     }
   }
 }
