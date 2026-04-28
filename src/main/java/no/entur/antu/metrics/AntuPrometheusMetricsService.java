9e7fb3b59bbb9b5bdd1658777c7fcd25d8a9c4f5
erlendev <erlend.vige@gmail.com>
2026-04-28 14:19:42 +0200

    Removes timeTakenToValidateDataSet metric

--- src/main/java/no/entur/antu/metrics/AntuPrometheusMetricsService.java
+++ src/main/java/no/entur/antu/metrics/AntuPrometheusMetricsService.java
@@ -1,16 +1,11 @@
 package no.entur.antu.metrics;
 
-import static no.entur.antu.Constants.REPORT_CREATION_DATE;
-
 import io.micrometer.core.instrument.ImmutableTag;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.camel.Handler;
-import org.apache.camel.Header;
 import org.entur.netex.validation.validator.ValidationReport;
 import org.springframework.stereotype.Component;
 
@@ -20,8 +15,6 @@ public class AntuPrometheusMetricsService {
   private static final String METRICS_PREFIX = "app.antu.";
   private static final String VALIDATION_ENTRIES_COUNTER_NAME =
     METRICS_PREFIX + "data.validation.entries";
-  private static final String VALIDATION_TIME_TAKEN_COUNTER_NAME =
-    METRICS_PREFIX + "data.validation.time.taken";
 
   private final MeterRegistry meterRegistry;
 
@@ -30,12 +23,8 @@ public class AntuPrometheusMetricsService {
   }
 
   @Handler
-  public void validationReportMetrics(
-    ValidationReport validationReport,
-    @Header(REPORT_CREATION_DATE) LocalDateTime reportCreationDate
-  ) {
+  public void validationReportMetrics(ValidationReport validationReport) {
     countValidationEntries(validationReport);
-    timeTakenToValidateDataSet(validationReport, reportCreationDate);
   }
 
   private void countValidationEntries(ValidationReport validationReport) {
@@ -51,21 +40,5 @@ public class AntuPrometheusMetricsService {
           .counter(VALIDATION_ENTRIES_COUNTER_NAME, counterTags)
           .increment(value);
       });
-  }
-
-  private void timeTakenToValidateDataSet(
-    ValidationReport validationReport,
-    LocalDateTime reportCreationDate
-  ) {
-    long seconds = Duration
-      .between(reportCreationDate, LocalDateTime.now())
-      .toSeconds();
-    List<Tag> counterTags = new ArrayList<>();
-    counterTags.add(
-      new ImmutableTag("Codespace", validationReport.getCodespace())
-    );
-    meterRegistry
-      .counter(VALIDATION_TIME_TAKEN_COUNTER_NAME, counterTags)
-      .increment(seconds);
   }
 }
