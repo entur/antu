afec7d60c0444b43adfda914408a75795af1e8c2
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-08-19 08:44:52 +0200

    Ignore references to VehicleType, for now. This is to enable plan data (ServiceJourney) to contain VehicleTypeRef even if there is currently no validation agains the vehicle registry. That validation will be created later on

---
+++ src/test/java/no/entur/antu/validation/validator/vehicletype/VehicleTypeIgnorerTest.java
@@ -0,0 +1,80 @@
+package no.entur.antu.validation.validator.vehicletype;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import no.entur.antu.config.ValidatorConfig;
+import no.entur.antu.validation.validator.id.ReferenceToNsrValidator;
+import org.entur.netex.validation.validator.ValidationIssue;
+import org.entur.netex.validation.validator.id.IdVersion;
+import org.entur.netex.validation.validator.id.NetexIdRepository;
+import org.entur.netex.validation.validator.id.NetexReferenceValidator;
+import org.entur.netex.validation.validator.jaxb.StopPlaceRepository;
+import org.entur.netex.validation.validator.xpath.XPathValidationContext;
+import org.entur.netex.validation.xml.NetexXMLParser;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class VehicleTypeIgnorerIntegrationTest {
+
+    @Test
+    void testServiceJourneyWithNonExistentVehicleTypeRefIsAccepted()  {
+        // Create the test NeTEx XML with a ServiceJourney that has a non-existent VehicleTypeRef
+        String netexXml = """
+      <?xml version="1.0" encoding="UTF-8"?>
+      <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
+        <dataObjects>
+          <CompositeFrame>
+            <frames>
+              <ServiceFrame>
+                <vehicleJourneys>
+                  <ServiceJourney id="TST:ServiceJourney:1" version="1">
+                    <VehicleTypeRef ref="TST:VehicleType:999"/>
+                  </ServiceJourney>
+                </vehicleJourneys>
+              </ServiceFrame>
+            </frames>
+          </CompositeFrame>
+        </dataObjects>
+      </PublicationDelivery>
+      """;
+
+        // Setup the validator components
+        NetexXMLParser parser = new NetexXMLParser(Set.of("SiteFrame"));
+        NetexIdRepository netexIdRepository = Mockito.mock(NetexIdRepository.class);
+        StopPlaceRepository stopPlaceRepository = Mockito.mock(StopPlaceRepository.class);
+        ReferenceToNsrValidator referenceToNsrValidator = new ReferenceToNsrValidator(stopPlaceRepository);
+
+        ValidatorConfig config = new ValidatorConfig();
+        NetexReferenceValidator validator = config.netexReferenceValidator(
+                netexIdRepository,
+                referenceToNsrValidator
+        );
+
+        // Parse the XML
+        var document = parser.parseByteArrayToXdmNode(netexXml.getBytes());
+        XPathValidationContext context = new XPathValidationContext(
+                document,
+                parser,
+                "TST",
+                null,
+                Set.of(),
+                List.of(new IdVersion("TST:VehicleType:999", null, "VehicleTypeRef", null, "Test.xml", 99, 34)),
+                "reportid-1"
+        );
+
+        // Run validation
+        List<ValidationIssue> issues = validator.validate(context);
+
+        // Assert that VehicleTypeRef does not produce a NETEX_ID_5 error
+        assertFalse(
+                issues.stream()
+                        .anyMatch(issue ->
+                                issue.rule().code().equals("NETEX_ID_5")
+                        ),
+                "VehicleTypeRef should be ignored and not produce NETEX_ID_5 errors"
+        );
+    }
+}
